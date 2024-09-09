f0643442dab187d1f54ccf2347caf28ee9ccd591
Patrick Benter <me@patrickbenter.com>
2024-09-09 21:03:46 +1000

    Update to material design 3 bottom navigation and add initial expandable fab components

--- lib/src/views/navigation.dart
+++ lib/src/views/navigation.dart
@@ -35,103 +35,31 @@ class _NavBarState extends State<NavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SlidingUpPanel(
-        maxHeight: 450.0, // TODO calculate
-        minHeight: 95.0,
-        controller: _panelController,
-        onPanelOpened: () => setState(() {
-          FocusScope.of(context).requestFocus(_energyInputFocus);
-        }),
-        parallaxEnabled: true,
-        parallaxOffset: 0.5,
-        body: SafeArea(
-          child: Scaffold(
-            body: _availableTabs.elementAt(_selectedTabIndex),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.dashboard),
+            label: "Overview",
           ),
-        ),
-        panel: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 12.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 30,
-                  height: 5,
-                  decoration: BoxDecoration(color: Colors.grey[300], borderRadius: const BorderRadius.all(Radius.circular(12.0))),
-                )
-              ],
-            ),
-            const SizedBox(height: 18.0),
-            BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                elevation: 0.0,
-                backgroundColor: Colors.white,
-                items: const [
-                  BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Overview"),
-                  BottomNavigationBarItem(icon: Icon(Icons.event_note), label: "History"),
-                  BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
-                ],
-                currentIndex: _selectedTabIndex,
-                selectedItemColor: Colors.red[800],
-                onTap: (int index) => setState(() => _selectedTabIndex = index)),
-            const SizedBox(height: 36.0),
-            Container(
-              padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-              child: Column(
-                children: [
-                  Form(
-                    key: _formKey,
-                    child: Row(
-                      children: [
-                        Consumer<INavigationViewModel>(
-                          builder: (context, model, child) {
-                            return Expanded(
-                              child: TextFormField(
-                                controller: _textEditingController,
-                                focusNode: _energyInputFocus,
-                                style: const TextStyle(fontSize: 24),
-                                textInputAction: TextInputAction.send,
-                                decoration: InputDecoration(
-                                  enabledBorder: const OutlineInputBorder(),
-                                  labelText: model.energyUnit == EnergyUnit.kj ? "Energy" : "Calories",
-                                  fillColor: Colors.white,
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15.0),
-                                  ),
-                                ),
-                                onFieldSubmitted: (energy) {
-                                  if (_formKey.currentState != null && _formKey.currentState!.validate()) {
-                                    model.energyAddRecord(double.parse(energy));
-
-                                    _textEditingController.clear();
-                                    _panelController.close();
-                                  }
-                                },
-                                validator: (value) => model.validator(value),
-                                keyboardType: TextInputType.number,
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-        borderRadius: const BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
-        onPanelSlide: (double pos) => setState(() {
-          if (pos < 0.9) {
-            FocusScope.of(context).unfocus();
-            FocusManager.instance.primaryFocus?.unfocus();
-          }
-        }),
+          NavigationDestination(
+            icon: Icon(Icons.event_note),
+            label: "History",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedTabIndex = index;
+          });
+        },
+        selectedIndex: _selectedTabIndex,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      ),
+      body: SingleChildScrollView(
+        child: _availableTabs[_selectedTabIndex],
       ),
     );
   }
