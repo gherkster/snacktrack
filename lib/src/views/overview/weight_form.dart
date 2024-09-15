2aab2182a040fb777995c3da72496e4c634b70d5
Patrick Benter <me@patrickbenter.com>
2024-09-15 22:26:15 +1000

    Style weight form and switch to decimal datepicker

--- lib/src/views/overview/weight_form.dart
+++ lib/src/views/overview/weight_form.dart
@@ -1,9 +1,9 @@
+import 'package:dart_date/dart_date.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:provider/provider.dart';
 import 'package:snacktrack/src/extensions/datetime.dart';
-import 'package:snacktrack/src/extensions/num.dart';
 import 'package:snacktrack/src/viewmodels/interfaces/i_overview_viewmodel.dart';
 
 class WeightForm extends StatefulWidget {
@@ -18,103 +18,125 @@ class WeightForm extends StatefulWidget {
 class _WeightFormState extends State<WeightForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  final fieldPadding = const EdgeInsets.symmetric(horizontal: 16);
+
   var date = DateTime.now().date;
   var time = TimeOfDay.now();
-  late double initialWeight = widget.currentWeight;
-
-  late int weightIntegerSegment = initialWeight.integer;
-  late int weightFractionSegment = initialWeight.fraction;
+  late double weight = widget.currentWeight;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+      ),
       body: Form(
         key: _formKey,
         child: Consumer<IOverviewViewModel>(
           builder: (context, model, child) {
             return Column(
               children: [
-                Text("Add weight"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Time"),
-                    Row(
-                      children: [
-                        TextButton(
-                          onPressed: () async {
-                            var selectedDate = await showDatePicker(
-                              context: context,
-                              initialDate: date,
-                              firstDate: model.today.copyWith(year: model.today.year - 1),
-                              lastDate: model.today,
-                            );
+                Padding(
+                  padding: fieldPadding.add(const EdgeInsets.only(bottom: 12)),
+                  child: Row(
+                    children: [Text("Add weight", style: Theme.of(context).textTheme.headlineMedium)],
+                  ),
+                ),
+                const Divider(),
+                Padding(
+                  padding: fieldPadding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Time"),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () async {
+                              var selectedDate = await showDatePicker(
+                                context: context,
+                                initialDate: date,
+                                firstDate: model.today.copyWith(year: model.today.year - 1),
+                                lastDate: model.today,
+                              );
 
-                            if (selectedDate == null) {
-                              return;
-                            }
+                              if (selectedDate == null) {
+                                return;
+                              }
 
-                            setState(() {
-                              date = selectedDate;
-                            });
-                          },
-                          child: Text(
-                            createDateLabel(date),
+                              setState(() {
+                                date = selectedDate;
+                              });
+                            },
+                            child: Text(
+                              createDateLabel(date),
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
                           ),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            var selectedTime = await showTimePicker(
-                              context: context,
-                              initialTime: time,
-                            );
+                          TextButton(
+                            onPressed: () async {
+                              var selectedTime = await showTimePicker(
+                                context: context,
+                                initialTime: time,
+                              );
 
-                            if (selectedTime == null) {
-                              return;
-                            }
+                              if (selectedTime == null) {
+                                return;
+                              }
 
-                            setState(() {
-                              time = selectedTime;
-                            });
-                          },
-                          child: Text(time.format(context)),
-                        )
-                      ],
-                    )
-                  ],
+                              setState(() {
+                                time = selectedTime;
+                              });
+                            },
+                            child: Text(
+                              time.format(context),
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Weight"),
-                    NumberPicker(
-                        minValue: model.weightMinSelectable,
-                        maxValue: model.weightMaxSelectable,
-                        value: weightIntegerSegment,
-                        onChanged: (value) => setState(() => weightIntegerSegment = value)),
-                    NumberPicker(
-                        minValue: 0,
-                        maxValue: 9,
-                        value: weightFractionSegment,
-                        onChanged: (value) => setState(() => weightFractionSegment = value)),
-                    Text("kg"),
-                  ],
+                const Divider(),
+                Padding(
+                  padding: fieldPadding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Weight"),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          DecimalNumberPicker(
+                            minValue: model.weightMinSelectable,
+                            maxValue: model.weightMaxSelectable,
+                            value: weight,
+                            onChanged: (value) => setState(() => weight = value),
+                            itemWidth: 64,
+                          ),
+                        ],
+                      ),
+                      Text(model.weightUnit.shortName),
+                    ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState?.validate() == true) {
-                          var combinedWeight = weightIntegerSegment + (weightFractionSegment.toDouble() / 10);
-                          model.addWeightRecord(combinedWeight, date.addTime(time));
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() == true) {
+                        model.addWeightRecord(weight, date.addTime(time));
 
-                          Navigator.pop(context);
-                        }
-                      },
-                      child: Text("Add weight"),
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Padding(
+                      padding: fieldPadding,
+                      child: const Text("Add weight"),
                     ),
-                  ],
+                  ),
                 ),
               ],
             );
@@ -125,11 +147,11 @@ class _WeightFormState extends State<WeightForm> {
   }
 
   String createDateLabel(DateTime dateTime) {
-    if (dateTime.date == DateTime.now().date) {
+    if (dateTime.isToday) {
       return "Today";
-    } else if (dateTime.date == DateTime.now().date.add(Duration(days: -1))) {
+    } else if (dateTime.isYesterday) {
       return "Yesterday";
     }
-    return DateFormat.yMMMd().format(dateTime);
+    return DateFormat.MMMd().format(dateTime);
   }
 }
