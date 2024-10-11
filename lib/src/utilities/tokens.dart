fa489e3dc03e390c868ea8349325c37d50a62e5c
Patrick Benter <me@patrickbenter.com>
2024-10-11 20:05:28 +1100

    Clean up food token generation

--- lib/src/utilities/tokens.dart
+++ lib/src/utilities/tokens.dart
@@ -1,12 +1,22 @@
+import 'package:snacktrack/src/extensions/iterable.dart';
+
 List<String> tokenize(String value) {
   return value
       .trim()
-      // Split by comma or space
-      .split(RegExp(r"[,\s]+"))
+      // Split by comma, space or forward slash
+      .split(RegExp(r"[,\s/]+"))
       .map(
         // Remove any parantheses surrounding alternate names like fetta (feta)
-        (v) => v.replaceAll("(", "").replaceAll(")", "").trim(),
+        (v) => v.replaceAll("(", "").replaceAll(")", "").trim().toLowerCase(),
       )
-      .where((v) => v.isNotEmpty)
+      .where((v) =>
+          v.isNotEmpty &&
+          !v.isIn([
+            "&",
+            "and",
+            "or",
+            "from",
+            "v", // comes from v/v when splitting by /
+          ]))
       .toList();
 }
